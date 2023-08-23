703ff83b55f939effee507e889fd182e45755c82
DEV <dev@step.rivne>
2023-08-23 11:44:56 +0300

    Implemented writing and reading methods

--- my_imp.py
+++ my_imp.py
@@ -1,29 +1,48 @@
 import os
 import csv
 
-
-def my_menu():
+def header():
     print("Welcome!!!")
     header = ['name', 'description', 'priority', 'data']
     filename="tasks.csv"
-
-    with open('tasks.csv', 'w') as csvfile:
+    with open('tasks.csv', 'w',newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(header)
+def add_task():
+    task= ["","","",""]
+    task[0]=input("Enter name: ")
+    task[1]=input("Enter description: ")
+    task[2]=input("Enter priority(high,medium,low): ")
+    task[3]=input("Enter date: ")
+    return task
+def write_to_file(obj):
+    with open('tasks.csv', '+a',newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(obj)
+def rewrite(edited_file):
+    with open('tasks.csv', '+a',newline='') as edited_file:
+        writer = csv.writer(edited_file)
+        writer.writerow(edited_file)
+def read_from_file():
+    with open('tasks.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for i in reader:
+            if i[0]=="2":
+                i.clear()
+        write_to_file(reader)
+        for i in reader:
+            print(i)
+
+  
+def my_menu():
     ex = False
     while ex == False:
         try:
             choise = int(input("1.Add new task.\n2.Show existing tasks.\n3.Delete task.\n4.Search a task.\n5.Edit task.\n0.Exit "))
             os.system('cls')
             if choise == 1:
-                t_name= ["","","",""]
-                t_name[0]=input("Enter name: ")
-                t_name[1]=input("Enter description: ")
-                t_name[2]=input("Enter priority(high,medium,low): ")
-                t_name[3]=input("Enter date: ")
-                with open('tasks.csv', '+a',newline='') as csvfile:
-                    writer = csv.writer(csvfile)
-                    writer.writerow(t_name)
+                write_to_file(add_task())
+                
                 os.system('cls')
             elif choise == 2:
                 next_choise=int(input("1.Show all tasks.\n2.Show tsks by month.\n3.Show task by week.\n4.Show task by day.\n0.Back to menu."))
--- start.py
+++ start.py
@@ -1,3 +1,7 @@
 import my_imp
 
+my_imp.header()
+
 my_imp.my_menu()
+
+my_imp.read_from_file()
